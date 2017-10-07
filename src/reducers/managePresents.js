e8fc11df1caf42e413620a4ab889a749b5c43da1
Pedro Acosta <pedro.acosta613@gmail.com>
2017-10-07 22:23:23 +0000

    automatically backed up by learn

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -2,10 +2,10 @@ export function managePresents(state, action){
 
   switch (action.type) {
     case "INCREASE":
-      return { numberOfPresents: state.numberOfPresents + 1 }
+      return { numberOfPresents: state.numberOfPresents + 1 };
       break;
     default:
-      return state
+      return state;
   }
 
 }
