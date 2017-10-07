462526e90d77d5eace01cbc778bc3bd573a0f26e
Pedro Acosta <pedro.acosta613@gmail.com>
2017-10-07 22:26:16 +0000

    automatically backed up by learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,7 +2,7 @@ export function manageFriends(state, action){
 
   switch (action.type) {
     case 'ADD_FRIEND':
-      return { friends: state.friends  }
+      return { friends: [...state.friends, action.friend]  }
       break;
     default:
       return state;
